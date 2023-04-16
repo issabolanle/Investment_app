ba6933dd00eed1976f9ac80a637fa4722727e8ed
issabolanle <issabolanle44@gmail.com>
2023-04-16 19:59:34 +0100

    first commit

--- capital.py
+++ capital.py
@@ -14,14 +14,16 @@ def investment():
     Account_name = (input('enter your account name: '))
     print('Welcome', Account_name, 'to cohort4 investment application')
     wallet =  int(input('enter your investment amount here: N'))
-    wallet_balance = wallet * (1 + (profit_percentage / 100))
+    interest = wallet * profit_percentage / 100
+    wallet_balance = wallet + interest
     print(Account_name, 'your wallet have been credited with the sum of: ','N', wallet_balance, 'with  a 50% interest on your capital')
     withdrawal_amount = int(input('enter amount you want to withdraw: N'))
     if withdrawal_amount >= wallet_balance:
        print(Account_name, 'your transaction failed due to insufficient fund')
     else:
         final_wallet_balance = wallet_balance - withdrawal_amount
-        final_balance = final_wallet_balance * (1 + (vat_percentage /100))
+        vat_reduction = final_wallet_balance * vat_percentage /100
+        final_balance = final_wallet_balance -vat_reduction
         print(Account_name, 'your withdrwal transaction was succesful and your account has been debited with:', 'N', withdrawal_amount, 'and a 7% service charge has been deducted and ','your final_balance is:', 'N', final_balance )
 
 
